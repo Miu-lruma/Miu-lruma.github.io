08b75f262c9c7b1f2f6032952d4a491200b22c4c
rkruege2 <rkruege2@nd.edu>
2017-09-20 13:41:55 -0400

    Going Live

--- javascripts/scrollScript.js
+++ javascripts/scrollScript.js
@@ -6,22 +6,22 @@ window.onscroll = function() {
 function scrollButtonColor() {
    var navbar = document.getElementById("myNavbar");
    var homeB = document.getElementById("homeButton");
-   var contactB = document.getElementById("contactButton");
+   var ResumeB = document.getElementById("ResumeButton");
    var portfolioB = document.getElementById("portfolioButton");
    var searchB = document.getElementById("searchButton");
    var aboutB = document.getElementById("aboutButton");
-   var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+   var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 1;
    if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
       navbar.className = "viice-bar";
       homeButton.className = "viice-bar-item viice-button  viice-verydark  viice-hover-main-color viice-text-white";
-      contactButton.className = "viice-bar-item viice-button  viice-verydark  viice-hover-main-color viice-text-white";
+      ResumeButton.className = "viice-bar-item viice-button  viice-verydark  viice-hover-main-color viice-text-white";
       portfolioButton.className = "viice-bar-item viice-button  viice-verydark  viice-hover-main-color viice-text-white";
       aboutButton.className = "viice-bar-item viice-button  viice-verydark  viice-hover-main-color viice-text-white";
    } else {
 
       navbar.className = "viice-bar";
       homeButton.className = "viice-bar-item viice-button  viice-main-color  viice-hover-verydark viice-text-aqua";
-      contactButton.className = "viice-bar-item viice-button  viice-main-color  viice-hover-verydark viice-text-aqua";
+      ResumeButton.className = "viice-bar-item viice-button  viice-main-color  viice-hover-verydark viice-text-aqua";
       portfolioButton.className = "viice-bar-item viice-button  viice-main-color  viice-hover-verydark viice-text-aqua";
       aboutButton.className = "viice-bar-item viice-button  viice-main-color  viice-hover-verydark viice-text-aqua";
    }
